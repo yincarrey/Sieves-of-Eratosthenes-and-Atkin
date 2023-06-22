a76a0715db99412b70533ade864face39afe838c
yincarrey <jean.agurton@utem.cl>
2023-06-22 02:29:30 -0400

    images

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@ int main() {
 
     // Calcular los números primos de forma paralela
     //criba de Atkin
-    /* std::vector<int> primos = obtenerPrimos(cribaAtkin(limite), limite); */
-    //criba de Eratostenes (opcional)
+    //std::vector<int> primos = obtenerPrimos(cribaAtkin(limite), limite);
+    //criba de Eratostenes
     std::vector<int> primos = obtenerPrimos(cribaDeEratostenes(limite), limite);
 
     // Mostrar los números primos
